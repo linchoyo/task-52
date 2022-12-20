d50b5d57990759c4ae3ecb261222c131bfadc5b2
Lin Yo <linchoyo@gmail.com>
2022-12-20 10:46:32 +0200

    Added on-click article elements.

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,21 @@
 import "../scss/app.scss";
 
+let a = document.querySelector("body");
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
-
   const button = document.querySelector(".button");
   button.addEventListener("click", () => {
     alert("💣");
   });
+
+  window.addEventListener("click", () => {
+    for (let i = 0; i < 5; i++)
+    {
+    let article = document.createElement("article");
+    article.classList.add("message");
+    article.textContent = "hi";
+    a.appendChild(article);
+    }
+  })
 });
