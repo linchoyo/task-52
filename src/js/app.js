594ccc1316eb9e4e808d1bab6629231c24c72ba2
Lin Yo <linchoyo@gmail.com>
2022-12-20 11:08:57 +0200

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -2,20 +2,42 @@ import "../scss/app.scss";
 
 let a = document.querySelector("body");
 
+let articleOne = document.createElement("article");
+articleOne.classList.add("message");
+articleOne.textContent = "hi";
+
+let articleTwo = document.createElement("article");
+articleTwo.classList.add("message");
+articleTwo.textContent = "hi";
+
+let articleThree = document.createElement("article");
+articleThree.classList.add("message");
+articleThree.textContent = "hi";
+
+let articleFour = document.createElement("article");
+articleFour.classList.add("message");
+articleFour.textContent = "hi";
+
+let articleFive = document.createElement("article");
+articleFive.classList.add("message");
+articleFive.textContent = "bb";
+
+a.addEventListener("click", () => {
+  
+  a.appendChild(articleOne);
+  a.appendChild(articleTwo);
+  a.appendChild(articleThree);
+  a.appendChild(articleFour);
+  a.appendChild(articleFive);
+})
+
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   const button = document.querySelector(".button");
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (event) => {
+    event.preventDefault();
+
     alert("💣");
   });
-
-  window.addEventListener("click", () => {
-    for (let i = 0; i < 5; i++)
-    {
-    let article = document.createElement("article");
-    article.classList.add("message");
-    article.textContent = "hi";
-    a.appendChild(article);
-    }
-  })
 });
+
